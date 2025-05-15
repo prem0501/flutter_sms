833a2643b41d571c12a81c684d6797a73c22575b
Prem <insomn.studio@gmail.com>
2025-05-15 14:39:57 +0530

    fixes

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.example
-
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.example.flutter_sms.FlutterSmsPlugin // Your plugin class
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        FlutterSmsPlugin.registerWith(flutterEngine.dartExecutor.binaryMessenger)
+    }
 }
